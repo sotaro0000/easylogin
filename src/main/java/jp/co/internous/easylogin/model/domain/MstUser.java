2f00dee0f45823a4c1be179a7a683f52a2fc210a
sotaro <sotaro.gt@gmail.com>
2025-05-23 19:22:14 +0900

    easylogin

---
+++ src/main/java/jp/co/internous/easylogin/model/domain/MstUser.java
@@ -0,0 +1,14 @@
+package jp.co.internous.easylogin.model.domain;
+
+public class MstUser {
+	
+	private int id;
+	private String userName;
+	private String password;
+	private String fullName;
+	public String getFullName() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
